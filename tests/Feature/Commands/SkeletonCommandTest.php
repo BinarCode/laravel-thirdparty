4600ed77ac511a0b3648db230cd86e9f68859a72
Eduard Lupacescu <lupacescueduard@gmail.com>
2020-06-08 14:20:02 +0300

    wip

--- tests/Feature/Commands/SkeletonCommandTest.php
+++ tests/Feature/Commands/SkeletonCommandTest.php
@@ -7,7 +7,7 @@ use Spatie\Skeleton\Tests\TestCase;
 class SkeletonCommandTest extends TestCase
 {
     /** * @test */
-    public function the_skeleton_command_workds()
+    public function the_skeleton_command_works()
     {
         $this->artisan('skeleton')->assertExitCode(0);
     }
