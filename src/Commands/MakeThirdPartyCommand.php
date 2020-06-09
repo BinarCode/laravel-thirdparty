bd6a328baf3730846cb24add6f9241f54b70e495
binaryk <binaryk@users.noreply.github.com>
2020-06-09 12:42:41 +0000

    Fix styling

--- src/Commands/MakeThirdPartyCommand.php
+++ src/Commands/MakeThirdPartyCommand.php
@@ -14,8 +14,8 @@ class MakeThirdPartyCommand extends Command
     {
         $this->comment('Creating Third Party...');
 
-/*From the UI: https://api.vacatures.co/api/candidate/v1/login/sso/redirect?provider=linkedin&job_board_id=1&redirect_job_board_url=https%3A%2F%2Fvacatures.co%2Faccount&isEmployer=false*/
-/*From FB: https://api.vacatures.co/api/candidate/v1/login/sso/handle?state=linkedin%2C1%2C0%2Chttps%3A%2F%2Fvacatures.co%2Faccount&code=AQQCJyuvjfhlXSPvNcuEMYdlUBcQZYdS-OBpaomqIh6iwg-9TOzbajHfUv07wOLorfN8dpks0NJkUPY54L0wC-fWX_iXgkckrx3JTj2ykpQFtxPz2TFM3sijw29cpypnX4k8mBJ6O1QOeO70NDGxZt1ynortDNi_eU_UMneueTjHcFuSP_TxKW-XWJJx1g*/
+        /*From the UI: https://api.vacatures.co/api/candidate/v1/login/sso/redirect?provider=linkedin&job_board_id=1&redirect_job_board_url=https%3A%2F%2Fvacatures.co%2Faccount&isEmployer=false*/
+        /*From FB: https://api.vacatures.co/api/candidate/v1/login/sso/handle?state=linkedin%2C1%2C0%2Chttps%3A%2F%2Fvacatures.co%2Faccount&code=AQQCJyuvjfhlXSPvNcuEMYdlUBcQZYdS-OBpaomqIh6iwg-9TOzbajHfUv07wOLorfN8dpks0NJkUPY54L0wC-fWX_iXgkckrx3JTj2ykpQFtxPz2TFM3sijw29cpypnX4k8mBJ6O1QOeO70NDGxZt1ynortDNi_eU_UMneueTjHcFuSP_TxKW-XWJJx1g*/
 
         $this->comment('Third party created!');
     }
--- src/Models/ThirdParty.php
+++ src/Models/ThirdParty.php
@@ -54,6 +54,4 @@ class ThirdParty extends Model
 
         return $this;
     }
-
-
 }
